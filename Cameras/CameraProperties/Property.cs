0860c57c485515b7eae37f5f09e7782e664e509a
vozoromnotna <87644340+vozoromnotna@users.noreply.github.com>
2024-11-12 13:19:26 +0300

    IsLogarithmic

--- Cameras/CameraProperties/Property.cs
+++ Cameras/CameraProperties/Property.cs
@@ -52,6 +52,8 @@ public abstract class Property<T> : PropertyBase
     public T MinValue { get { return _minValue; } }
     public T MaxValue { get { return _maxValue; } }
 
+    protected bool _isLogarithmic;
+    public bool IsLogarithmic { get => _isLogarithmic; }
 
     protected T _value;
     public T Value
