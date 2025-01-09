9bede76754ac8e2f5f56b44670fb1c315f88f081
vozoromnotna <87644340+vozoromnotna@users.noreply.github.com>
2025-01-09 15:25:55 +0300

    com controller base. Start utc12L

---
+++ Cameras/CameraProperties/COMController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace AsyncCapture.Cameras.CameraProperties
+{
+    public abstract class COMControllerBase
+    {
+        const int _serialTimeout = 25;
+        static readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        const int MaxTries = 3;
+        SerialPort _serialPort;
+
+        public COMControllerBase(string comPortName)
+        {
+            OpenSerialPort(comPortName);
+        }
+
+        protected abstract SerialPort OpenSerialPort(string comPortName);
+        protected abstract byte[] GetTestRequest();
+        public virtual async Task<bool> TestPort()
+        {
+            var request = GetTestRequest();
+            var responce  = await ReciveResponce(request);
+            return IsCorrect(responce);
+
+        }
+        public async Task SendRequest(byte[] request)
+        {
+            await SendRequest(_serialPort, request);
+        }
+
+        public async Task<byte[]> ReciveResponce(byte[] request)
+        {
+            return await ReciveMessage(_serialPort, request);
+        }
+        public async Task<byte[]> ReciveMessage(SerialPort serialPort, byte[] message)
+        {
+            int tries = 0;
+            await _semaphore.WaitAsync();
+            var result = new List<byte>();
+            while (true)
+            {
+                result.Clear();
+                serialPort.Write(message, 0, message.Length);
+
+                await Task.Delay(_serialTimeout);
+
+                while (serialPort.BytesToRead > 0)
+                {
+                    var readRes = serialPort.ReadByte();
+                    if (readRes > 0)
+                    {
+                        result.Add((byte)readRes);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                if (!IsCorrect(result?.ToArray()) && tries < MaxTries)
+                {
+                    tries++;
+                    await Task.Delay(_serialTimeout);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            _semaphore.Release();
+            return result.ToArray();
+        }
+
+        protected abstract bool IsCorrect(byte[]? bytes);
+
+        public async Task SendRequest(SerialPort serialPort, byte[] message)
+        {
+            await _semaphore.WaitAsync();
+            var result = new List<byte>();
+
+            result.Clear();
+
+            serialPort.Write(message, 0, message.Length);
+
+            await Task.Delay(_serialTimeout);
+
+            _semaphore.Release();
+        }
+
+    }
+}
---
+++ Cameras/CameraProperties/UTC12LProperties/UTC12LComController.cs
@@ -0,0 +1,92 @@
+﻿using DirectShowLib.BDA;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+
+namespace AsyncCapture.Cameras.CameraProperties.UTC12LProperties
+{
+    internal class UTC12LComController : COMControllerBase
+    {
+        private const byte DeviceAddress = 0x26;
+        private const byte StartByte = 0xF0;
+        private const byte EndByte = 0xFF;
+        public UTC12LComController(string comPortName) : base(comPortName)
+        {
+        }
+
+        protected override byte[] GetTestRequest()
+        {
+            return new byte[]
+            {
+                0xF0,
+                0x02,
+                0x26,
+                0x00,
+                0x26,
+                0xFF
+            };
+        }
+
+        protected override bool IsCorrect(byte[]? bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override SerialPort OpenSerialPort(string comPortName)
+        {
+            return new SerialPort(comPortName, 115200);
+        }
+
+        private byte[] CreateRequest(byte instruction, byte[] data = null)
+        {
+            List<byte> requestData = [instruction, DeviceAddress];
+
+            if (data != null)
+            {
+                data = ProcessData(data);
+                requestData.AddRange(data);
+            }
+
+            byte checkSum = CalculateCheckSum(requestData);
+            List<byte> request = [StartByte, (byte)requestData.Count];
+            request.AddRange(requestData);
+            request.Add(checkSum);
+            request.Add(EndByte);
+
+            return request.ToArray();
+        }
+
+        private byte CalculateCheckSum(List<byte> data)
+        {
+            return (byte)data.Sum(x => x);
+        }
+
+        private byte[] ProcessData(byte[] data) 
+        {
+            List<byte> processedData = new();
+            foreach (byte b in data)
+            {
+                switch (b)
+                {
+                    case 0xF0:
+                        processedData.Add(0xF5);
+                        processedData.Add(0x00);
+                        break;
+                    case 0xFF:
+                        processedData.Add(0xF5);
+                        processedData.Add(0x0F);
+                        break;
+                    case 0xF5:
+                        processedData.Add(0xF5);
+                        processedData.Add(0x05);
+                        break;
+                }
+            }
+            return processedData.ToArray();
+        } 
+    }
+}
