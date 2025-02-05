510b8620a3992d0377a2ae57892d0e8a5bec7ef2
vozoromnotna <87644340+vozoromnotna@users.noreply.github.com>
2025-02-05 14:26:36 +0300

    Revert "VioReader"
    This reverts commit 58823c38bd7d77a42cc6756f6fd6ce7b892a3705.

--- Cameras/CameraProperties/IrayProperties/IrayComAdapter.cs
+++ Cameras/CameraProperties/IrayProperties/IrayComAdapter.cs
@@ -18,7 +18,7 @@ namespace AsyncCapture.Cameras.CameraProperties.IrayProperties
     public class IrayComDeviceInfo : ComDeviceInfo
     {
         readonly static HashSet<byte> ErrorCodes = [0xF1, 0xFB, 0xFD, 0xFF];
-        public IrayComDeviceInfo() : base("IrayCom") 
+        public IrayComDeviceInfo() 
         {
             _baundRate = 115200;
             _validator = new SimpleResponseValidator(Validator);
--- Cameras/CameraProperties/UTC12LProperties/UTC12LComAdapter.cs
+++ Cameras/CameraProperties/UTC12LProperties/UTC12LComAdapter.cs
@@ -17,7 +17,7 @@ namespace AsyncCapture.Cameras.CameraProperties.UTC12LProperties
     public class UTC12LComDeviceInfo : ComDeviceInfo
     {
 
-        public UTC12LComDeviceInfo() : base("UTC12LCom")
+        public UTC12LComDeviceInfo()
         {
             _baundRate = 115200;
             _validator = new SimpleResponseValidator(Validator);
