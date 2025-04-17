8047e9223c6e06dc92f0d2499439543c9b394e48
vozoromnotna <87644340+vozoromnotna@users.noreply.github.com>
2025-04-17 12:20:05 +0300

    test camera source

---
+++ Cameras/CameraSources/TestCS.cs
@@ -0,0 +1,149 @@
+﻿using Gst;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Task = System.Threading.Tasks.Task;
+
+namespace AsyncCapture.Cameras.CameraSources
+{
+    public class TestCS : CameraSource
+    {
+        public TestCS()
+        {
+
+        }
+
+        public int Width { get; set; } = 1920;
+        public int Height { get; set; } = 1080;
+        public double Fps { get; set; } = 60.0;
+        public MatType MatType { get; set; } = MatType.CV_8UC1;
+        public override string Name => "TestSource";
+
+        private bool _isLive = true;
+        public override bool IsLive { get => _isLive; }
+
+
+
+        CancellationTokenSource _cts;
+        public void StartImage()
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+            Task.Run(async () =>
+            {
+                int r = 1;
+                int increment = 5;
+                int directionX = 1;
+                int xSpeed = 10;
+                int directionY = 1;
+                int ySpeed = 8;
+                int phase = 0;
+                int phaseInc = 10;
+                int color = 0;
+                int colorMaxCount = (int)Fps;
+                int colorCount = 0;
+                int x = Height / 2;
+                int y = Width / 2;
+
+                while (!token.IsCancellationRequested)
+                {
+                   // var waitTask = Task.Delay((int)((1.0 / Fps) * 1000));
+                    var image = new Mat(Height, Width, MatType);
+                    var channels = image.Channels();
+                    var maxIntensity = Math.Pow(2, (image.ElemSize()/channels) * 8) - 1;
+                    double[,] imageData = new double[Height, Width];
+                    Parallel.For(0, image.Rows, (i) =>
+                    {
+                        for (int j = 0; j < image.Cols; j++)
+                        {
+                            var value = (Math.Cos(Math.PI * (j - phase) / image.Cols) + 1) * maxIntensity / 2.0;
+                            imageData[i, j] = value;
+                        }
+                    });
+
+                    var tempMat = Mat.FromArray(imageData);
+                    tempMat.ConvertTo(image, MatType);
+                    if (channels == 3)
+                    {
+                        Cv2.CvtColor(image, image, ColorConversionCodes.GRAY2BGR);
+                    }
+                    
+
+                    phase += phaseInc;
+
+                    Scalar colorScalar = Scalar.All(maxIntensity);
+
+                    if (MatType == MatType.CV_8UC3)
+                    {
+                        var blue = (color & 0xFF0000) >> 16;
+                        var green = (color & 0x00FF00) >> 8;
+                        var red = color & 0x0000FF;
+
+                        colorCount += 1;
+                        if (colorCount > colorMaxCount)
+                        {
+                            color = new Random().Next(0xFFFFFF);
+                        }
+                        colorScalar = Scalar.FromRgb(red, green, blue);
+                    }
+
+                    Cv2.Circle(image, new Point(x, y), r, colorScalar, -1);
+
+                    if (x + r >= image.Cols)
+                    {
+                        directionX = -1;
+                    }
+
+                    if (x - r <= 0)
+                    {
+                        directionX = 1;
+                    }
+
+                    if (y + r >= image.Rows)
+                    {
+                        directionY = -1;
+                    }
+
+                    if (y - r <= 0)
+                    {
+                        directionY = 1;
+                    }
+
+                    x += directionX * xSpeed;
+                    y += directionY * ySpeed;
+                    r += increment;
+                    if (r > 100 || r <= Math.Abs(increment))
+                    {
+                        increment *= -1;
+                    }
+                    Dictionary<string, object> meta = new();
+                    await imageGetted(image, meta);
+                }
+
+            }, token);
+        }
+
+
+        public override void Dispose()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+        }
+
+        public override void StartLive()
+        {
+            _isLive = true;
+            StartImage();
+        }
+
+        public override void StopLive()
+        {
+            _isLive = false;
+            _cts?.Cancel();
+        }
+    }
+}
